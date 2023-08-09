e4e71361a693d1b6dff0a01a7558ea0dbb1ef86d
nayandrakshapally <nayan.drakshapally@gmail.com>
2023-08-09 11:14:52 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const heading = React.createElement(
+  "h1",
+  { id: "firstHeading" },
+  "This is first react app!"
+);
+// root.render(heading);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is nested element1"),
+    React.createElement("h2", {}, "This is nested element2"),
+  ])
+);
+root.render(parent);
